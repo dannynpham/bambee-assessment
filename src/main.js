fb4000faf5389a2175276b361fc253c61649e8f9
Danny Pham <dannynpham@csu.fullerton.edu>
2021-01-24 17:27:22 -0800

    globally registered components

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue';
+import * as pollens from '@bambee/pollen';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+Object.entries(pollens).forEach(([name, component]) => {
+  if (name !== 'constants') {
+    Vue.component(name, component);
+  }
+});
 
 new Vue({
   router,
